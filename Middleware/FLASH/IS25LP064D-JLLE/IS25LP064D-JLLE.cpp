20afcf37485b6908c20b643f0036d1e5284da642
Maidui3 <158636271+Maidui3@users.noreply.github.com>
2026-04-20 23:23:01 +0900

    cmoit

---
+++ Middleware/FLASH/IS25LP064D-JLLE/IS25LP064D-JLLE.cpp
@@ -0,0 +1,9 @@
+
+#include "IS25LP064D-JLLE.hpp"
+
+namespace maidui3_hal {
+namespace Middleware {
+namespace IS25LP064D_JLLE {
+}
+}  // namespace Middleware
+}  // namespace maidui3_hal
---
+++ Middleware/FLASH/IS25LP064D-JLLE/IS25LP064D-JLLE.hpp
@@ -0,0 +1,15 @@
+
+#pragma once
+#include <cstdint>
+
+namespace maidui3_hal {
+namespace Middleware {
+namespace IS25LP064D_JLLE {
+
+class is25lp064d_jlle
+{
+};
+
+}  // namespace IS25LP064D_JLLE
+}  // namespace Middleware
+}  // namespace maidui3_hal
