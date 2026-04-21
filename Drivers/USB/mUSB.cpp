7ad44c3dbb73dfbdc9cb6f090d180204f8731b2a
Maidui3 <lifelong1027@gmail.com>
2026-04-21 18:49:43 +0900

    comit 4/21

---
+++ Drivers/USB/mUSB.cpp
@@ -0,0 +1,114 @@
+
+#include "mUSB.hpp"
+
+namespace maidui3_hal {
+namespace Drivers {
+namespace USB {
+
+bool usb::begin(const int __speed)
+{
+    HAL_PCD_Start(husbx_);
+
+    HAL_PCD_DevConnect(husbx_);
+
+    return 0;
+}
+
+bool usb::print(const char* __format)
+{
+    return 0;
+}
+
+bool usb::print(const signed char __data_s8)
+{
+    return 0;
+}
+
+bool usb::print(const unsigned char __data_u8)
+{
+    return 0;
+}
+
+bool usb::print(const signed short __data_s16)
+{
+    return 0;
+}
+
+bool usb::print(const unsigned short __data_s16)
+{
+    return 0;
+}
+
+bool usb::print(const signed int __data_s32)
+{
+    return 0;
+}
+
+bool usb::print(const unsigned int __data_u32)
+{
+    return 0;
+}
+
+bool usb::print(const signed long long __data_s64)
+{
+    return 0;
+}
+
+bool usb::print(const unsigned long long __data_u64)
+{
+    return 0;
+}
+
+bool usb::print(float __float)
+{
+    return 0;
+}
+
+bool usb::print(double __double)
+{
+    return 0;
+}
+
+bool usb::end()
+{
+    if (HAL_PCD_Stop(husbx_) != HAL_OK) {
+        return 1;
+    }
+
+    while (HAL_PCD_DevDisconnect(husbx_) != HAL_OK);
+
+    return 0;
+}
+bool usb::getMessage()
+{
+    return 0;
+}
+
+}  // namespace USB
+}  // namespace Drivers
+}  // namespace maidui3_hal
+
+extern "C" {
+
+void HAL_PCD_SOFCallback(PCD_HandleTypeDef* hpcd) {}
+
+void HAL_PCD_SetupStageCallback(PCD_HandleTypeDef* hpcd) {}
+
+void HAL_PCD_ResetCallback(PCD_HandleTypeDef* hpcd) {}
+
+void HAL_PCD_SuspendCallback(PCD_HandleTypeDef* hpcd) {}
+
+void HAL_PCD_ResumeCallback(PCD_HandleTypeDef* hpcd) {}
+
+void HAL_PCD_ConnectCallback(PCD_HandleTypeDef* hpcd) {}
+
+void HAL_PCD_DisconnectCallback(PCD_HandleTypeDef* hpcd) {}
+
+void HAL_PCD_DataOutStageCallback(PCD_HandleTypeDef* hpcd, uint8_t epnum) {}
+
+void HAL_PCD_DataInStageCallback(PCD_HandleTypeDef* hpcd, uint8_t epnum) {}
+
+void HAL_PCD_ISOOUTIncompleteCallback(PCD_HandleTypeDef* hpcd, uint8_t epnum) {}
+
+void HAL_PCD_ISOINIncompleteCallback(PCD_HandleTypeDef* hpcd, uint8_t epnum) {}
+}
