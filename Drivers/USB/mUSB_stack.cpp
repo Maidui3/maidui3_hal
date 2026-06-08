5227d531ceb43b8858a898869e1805cc3a614ac4
Maidui3 <158636271+Maidui3@users.noreply.github.com>
2026-06-09 01:20:15 +0900

    add

--- Drivers/USB/mUSB_stack.cpp
+++ Drivers/USB/mUSB_stack.cpp
@@ -39,10 +39,30 @@ bool usb_stack::stop()
     return 0;
 }
 
-void usb_stack::SOF_Callback(PCD_HandleTypeDef* husb_pcd__) {}
+void usb_stack::SOF_Callback(PCD_HandleTypeDef* husb_pcd__)
+{
+    // SOF
+    // 0000 0001 1010 0101 1011 0111 101 10011
+    /* |  SYNC | |  PID  | |   FRAME   | |CRS| */
+
+    // Setup Token
+    // 0000 0001 1011 0100 0000 0000 000 01000
+    /* |  SYNC | |  PID  | | ADDR || EN ||CRS| */
+
+    // Setup Data
+    // 0000 0001 1100 0011 0000 0001 0110 0000 0000 0000 1000 0000 0000 0000 0000 0000 0000 0010 0000 0000
+    /* |  SYNC | |  PID  | | 1Byte | | 2Byte | | 3Byte | | 4Byte | | 5Byte | | 6Byte | | 7Byte | | 8Byte | */
+    // 1011 1011 0010 1001
+    /* |       CRS       | */
+
+    // Setup Handshake
+    // 0000 0001 0100 1011
+    /* |  SYNC | |  PID  |*/
+}
 
 void usb_stack::SetupStage_Callback(PCD_HandleTypeDef* husb_pcd__)
 {
+    HAL_GPIO_WritePin(LED_Wio_E5_GPIO_Port, LED_Wio_E5_Pin, GPIO_PIN_SET);
     /**
      * 1Byte : bmRequestType
      * 1Byte : bRequest
@@ -50,6 +70,18 @@ void usb_stack::SetupStage_Callback(PCD_HandleTypeDef* husb_pcd__)
      * 2Byte : wIndex
      * 2Byte : wLength
      */
+
+    Transmit_Control_Stage_Buffer[0] = 0x03;
+    Transmit_Control_Stage_Buffer[1] = 0x01;
+    Transmit_Control_Stage_Buffer[2] = 0x00;
+    Transmit_Control_Stage_Buffer[3] = 0x02;
+    Transmit_Control_Stage_Buffer[4] = 0x00;
+    Transmit_Control_Stage_Buffer[5] = 0x00;
+    Transmit_Control_Stage_Buffer[6] = 0x00;
+    Transmit_Control_Stage_Buffer[7] = 0x08;
+
+    HAL_PCD_EP_Transmit(husb_pcd__, PCD_ENDP0, _USB_Stack_.Transmit_Control_Stage_Buffer, 8);
+
     if (USB_PCD_FS.hpcd__ == husb_pcd__) {
         if (!(husb_pcd__->Setup[0] & bmRequestType_Dir_MSK)) {
             // Host -> Device
